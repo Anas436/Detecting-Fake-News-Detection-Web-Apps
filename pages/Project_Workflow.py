5d10b921e295ee346a0a789e37e810f102655b54
dots_13 <stasya.dots@gmail.com>
2023-09-07 03:59:45 +0200

    add workflow picture

--- pages/Project_Workflow.py
+++ pages/Project_Workflow.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import base64
+from PIL import Image
 
-def st_display_pdf(pdf_file):
-    with open(pdf_file,"rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<embed src=”data:application/pdf;base64,{base64_pdf}” width=”700″ height=”1000″ type=”application/pdf”>’
-    st.markdown(pdf_display, unsafe_allow_html=True)
-    
-(st_display_pdf('../pictures/Project-workflow.pdf'))
+image = Image.open('../pictures/workflow.PNG')
+
+st.image(image, caption='Sunrise by the mountains')
+   
