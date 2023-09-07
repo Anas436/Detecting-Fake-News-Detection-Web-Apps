4a63c682eca7b6d7d1e28b56395ffa07cd036635
dots_13 <stasya.dots@gmail.com>
2023-09-07 02:29:08 +0200

    add models

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
 import streamlit as st
 import base64
+import sklearn
+import pickle
+import clean
 
 st.set_page_config(page_title='Title',
                    page_icon=':globe_with_meridians:',
                    layout='centered')
-
-st.header('Fake News Classification.', anchor=False)
-
-text_user = st.text_area(label='text_label',
-                         value='Insert a news title here.',
-                         disabled=False,
-                         label_visibility='hidden')
-
-
-run_button = st.button('Classify')
-if run_button:
-    st.text('True: 0.6')
-    st.text('False: 0.4')
-else:
-    st.text('Classification results.')
-
+                   
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
@@ -49,4 +37,24 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+xgb_model_loaded = pickle.load(open('models/Best_LGBMClassifier_Final_model_X1.pkl', "rb"))
+tf_idf = pickle.load(open('models/Best_tfidf_vectorizer_Final_model_X1.pkl', "rb"))
+
+st.header('Fake News Classification.', anchor=False)
+
+text_user = st.text_area(label='text_label',
+                         value='Insert a news title here.',
+                         disabled=False,
+                         label_visibility='hidden')
+
+
+run_button = st.button('Classify')
+if run_button:
+    st.text('True: 0.6')
+    st.text('False: 0.4')
+else:
+    st.text('Classification results.')
+
 
