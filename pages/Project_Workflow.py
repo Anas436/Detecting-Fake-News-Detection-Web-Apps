951668991d1ae347ad64267898eac2b456a2ce51
dots_13 <stasya.dots@gmail.com>
2023-09-07 04:12:24 +0200

    add workflow picture

--- pages/Project_Workflow.py
+++ pages/Project_Workflow.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 from pathlib import Path
 
-image = Image.open('./pictures/workflow.PNG')
+image = Image.open('./pictures/workflow.png')
 
 st.image(image, caption='Sunrise by the mountains')
    
