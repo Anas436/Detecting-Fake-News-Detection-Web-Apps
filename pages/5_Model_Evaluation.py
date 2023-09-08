fe52fdc1b2eabd45505eee5ad26bae5a7d80c0a7
dots_13 <stasya.dots@gmail.com>
2023-09-08 18:42:40 +0200

    add table

--- pages/5_Model_Evaluation.py
+++ pages/5_Model_Evaluation.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Model Evaluation', anchor=None)
 st.write('The culmination of our model development efforts led to an impressive performance in the evaluation phase. The final evaluation of the model yielded the following results:')
@@ -16,6 +17,18 @@ st.markdown(
 st.write("These metrics highlight the exceptional capabilities of our model in distinguishing between genuine and fake news articles. It showcases a remarkable accuracy rate of 99%, underlining the model's ability to make correct predictions. Additionally, a precision score of 1.00 signifies the identification of fake news, with very few false positives. The recall score of 0.99 underscores the model's proficiency in capturing almost all instances of fake news within the dataset. The F1-score, which balances precision and recall, solidifies the overall robustness of our model.")
 st.subheader('Classification Report')
 st.write("To further dissect the model's performance, we provide a comprehensive classification report:")
+
+# initialize data of lists.
+data = {'precision ': ['0.98', '1.00', ' ', '0.99', '0.99'],
+        'recall ': ['0.98', '0.99', ' ', '0.99', '0.99'],
+        'f1-score': ['0.98', '0.99', '0.99', '0.99', '0.99'],
+        'support': ['1129', '5350', '6479', '6479', '6479']
+        }
+  
+# Create DataFrame
+df = pd.DataFrame(data, index=[0, 1, 'accuracy', 'macro avg', 'weighted avg'])
+st.write(df)
+
 st.markdown(
 """
 | q | precision | recall | f1-score | support|
